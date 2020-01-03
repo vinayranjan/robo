6635fb9f16830fd542e49bfb15bbb80e2d629903
Vinay Ranjan <>
2020-01-04 00:21:30 +0530

    ADD: basic util functions required

--- utils.py
+++ utils.py
@@ -1,1 +1,19 @@
 """All the required modules/functions available here."""
+
+def robo_move(direction='forward'):
+	'''Responsible for moving the robo.
+	   direction can be both forward and reverse
+	'''
+	return true
+
+def _control_servo():
+	'''Control the movement/ angle of servo.'''
+	return True
+
+def detect_obstacle_dist():
+	'''Calculate the distance of obstacle.'''
+	return True
+
+def stop_robo():
+	'''Stop the movement of robo.'''
+	return True
