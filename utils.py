05bec8e56bf26e3bba1ac81b6bd91872ba5064ff
Parashar Sangle <parashar.sangle@gramener.com>
2020-01-11 15:16:54 +0530

    ENH: collision ddetecttion

--- utils.py
+++ utils.py
@@ -82,7 +82,7 @@ def detect_obstacle_dist():
                     # else:
                     #     __stop()
                     #     print("do something here")
-                    print(dist, angle_history)
+                    print("if", dist, angle_history)
                 else:
                     if angle == 2:
                         if angle_history[0] > angle_history[1]:
